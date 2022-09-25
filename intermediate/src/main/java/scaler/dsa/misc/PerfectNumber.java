ee3561333bb7884d2d2f48fa9f76fcd2ac5e3b3e
bejjanki <rajesh.bejjanki@gmail.com>
2022-09-25 23:23:20 +0530

    Perfect Number Problem

--- intermediate/src/main/java/scaler/dsa/misc/PerfectNumber.java
+++ intermediate/src/main/java/scaler/dsa/misc/PerfectNumber.java
@@ -1,58 +1,49 @@
 package scaler.dsa.misc;
 
-/*  Given a Number find out if its prime number or not
-    A Number is prime, if it has the following properties
-    1) 1 and itself are factors
-    2) Number of factors are two
-* */
+/*  For a given integer N, you have to tell whether it is a perfect number or not.
+    A perfect number is a positive integer which is equal to the sum of its proper positive divisors.
+ */
 
 class PerfectNumber {
 
     public static void main(String[] args) {
-        System.out.print("Is number 7 is prime : " + isPrimeNumber(7));
-        System.out.print("Is number 7 is prime : " + isPrimeNumber(173));
-        System.out.print("Is number 7 is prime : " + isPrimeNumber(563));
 
-        System.out.print("Is number 7 is prime : " + isPrimeOptimised(7));
-        System.out.print("Is number 7 is prime : " + isPrimeOptimised(173));
-        System.out.print("Is number 7 is prime : " + isPrimeOptimised(563));
-
+        System.out.print(isPerfectNumber(4));
+        System.out.print(isPerfectNumber(6));
     }
 
     /*  Brute Force Approach:
-        Iterate through 1 to N and count number of factors. if factors are equals to 2 then its prime.
+        Iterate from 1 to N and add each number that divides given number to the sum. Finally, check if sum is equals to given number.
+
         Time Complexity  : O(N)
         Space Complexity : O(1)
-     */
-    public static boolean isPrimeNumber(int N) {
-        int count = 0;
-        for (int i = 1; i <= N; i++) {
+    */
+    public static boolean isPerfectNumber(int N) {
+        int sum = 1;
+        for (int i = 2; i * i <= N; i++) {
             if (N % i == 0) {
-                count++;
+                sum += i;
+                sum += N/i;
             }
         }
-        return count == 2;
+        return sum == N;
     }
 
     /*
-        The factors for a number always appears in pairs(i, N/i). So we don't have to check all the factors.
-        If i is a factor of N then N/i is also a factor. So we can loop from 1 to Sqrt(N) to check if it's a prime or not.
+      Since factors always comes in pair(i, N/i), iterate from 1 to Sqrt(n) and add to sum.
+      Finally, check if sum is equals to given number.
 
-        Time  Complexity : O(Sqrt(N))
-        Space Complexity : O(1)
-     */
-    public static boolean isPrimeOptimised(int N) {
-        int count = 0;
-        for (int i = 1; i * i <= N; i++) {
+      Time Complexity  : O(Sqrt(N))
+      Space Complexity : O(1)
+    */
+    public static boolean isPerfectNumberOptimised(int N) {
+        int sum = 1;
+        for (int i = 2; i * i <= N; i++) {
             if (N % i == 0) {
-                // if N is a perfect square like 36 then (6, 36/6)=> (6,6) is a pair but we want to consider 6 only once. so this case will handle perfect square scenario.
-                if (i == N / i) {
-                    count++;
-                } else {
-                    count += 2;
-                }
+                sum += i;
+                sum += N/i;
             }
         }
-        return count == 2;
+        return sum == N;
     }
 }
